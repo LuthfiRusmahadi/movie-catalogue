07bac8d938dc81563fded94c611ab363a010ab66
LuthfiRusmahadi <luthfirus9d@gmail.com>
2023-06-07 15:04:54 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '7e3e8809e44213b9915b60b187257a34',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
